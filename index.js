0bf0390410e6ee82e4549e95f9f4313ab450a6d0
bestPhumipat <ppbesto.9945@gmail.com>
2023-04-10 16:09:45 +0700

    check process.env

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const addRouter = require("./routes/add");
 const getRouter = require("./routes/get");
 
 const { DB_URL, PORT } = process.env;
-console.log(DB_URL, POR);
+console.log(process.env);
 
-mongoose.connect(DB_URL, {
-  useNewUrlParser: true
-});
+// mongoose.connect(DB_URL, {
+//   useNewUrlParser: true
+// });
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-app.use("/add", addRouter);
-app.use("/get", getRouter);
+// app.use("/add", addRouter);
+// app.use("/get", getRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
