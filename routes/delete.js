5156fafea3528c8e6140d011676702b8b6c093be
bestPhumipat <ppbesto.9945@gmail.com>
2023-04-11 22:48:17 +0700

    fix delete API

--- routes/delete.js
+++ routes/delete.js
@@ -9,10 +9,11 @@ router.delete("/:AssetNo/:Type", async (req, res) => {
   try {
     const assetNo = req.params.AssetNo;
     const type = req.params.Type;
-
-    const data = await Asset.find();
+    console.log(assetNo, type);
+    const data = await Asset.find({ Type: type });
     if (data.length === 1) {
-      Type.deleteOne({ Type: type });
+      console.log(data);
+      await Type.deleteOne({ name: type });
     }
     await Asset.deleteOne({ AssetNo: assetNo });
     return res.json({
