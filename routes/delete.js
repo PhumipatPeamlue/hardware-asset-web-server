2c6e70883dc9526e688bea2162c3583dd1abb0a8
bestPhumipat <ppbesto.9945@gmail.com>
2023-04-11 22:30:40 +0700

    fix delete asset API that can auto delete it's type

--- routes/delete.js
+++ routes/delete.js
@@ -1,12 +1,19 @@
 const express = require("express");
 
 const Asset = require("../models/asset");
+const Type = require("../models/type");
 
 const router = express.Router();
 
-router.delete("/:AssetNo", async (req, res) => {
+router.delete("/:AssetNo/:Type", async (req, res) => {
   try {
     const assetNo = req.params.AssetNo;
+    const type = req.params.Type;
+
+    const data = await Asset.find();
+    if (data.length === 1) {
+      Type.deleteOne({ Type: type });
+    }
     await Asset.deleteOne({ AssetNo: assetNo });
     return res.json({
       msg: "delete asset successfully!"
